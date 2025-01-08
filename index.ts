753dc8b270ffd1797ff0781ea95609e8eef76f80
Mark Milligan <mtm20176@gmail.com>
2025-01-08 08:46:02 -0600

    chore: bun run start at container start and add hello to greeting

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import figlet from "figlet";
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        const body = figlet.textSync("Bun!");
+        const body = figlet.textSync("Hello, Bun!");
         return new Response(body);
     },
   });
