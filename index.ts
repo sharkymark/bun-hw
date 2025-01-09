ae6081b1cf4d991fc7c55e94af50d3877b01ae1e
Mark Milligan <mtm20176@gmail.com>
2025-01-08 21:28:40 -0600

    chore: add logic to take user input

--- index.ts
+++ index.ts
@@ -1,10 +1,43 @@
 import figlet from "figlet";
 
+let requestCounter = 0;
+
 const server = Bun.serve({
     port: 3000,
-    fetch(req) {
-        const body = figlet.textSync("Hello, Bun!");
-        return new Response(body);
+    async fetch(req) {
+        requestCounter++;
+        console.log(`Web request #${requestCounter} received`);
+        if (req.method === "POST") {
+            const formData = await req.formData();
+            const userInput = formData.get("input") || "";
+            const body = figlet.textSync(`Hello, ${userInput} - You're using Bun! `);
+            const html = `
+                <html>
+                    <body>
+                        <pre>${body}</pre>
+                        <form method="POST">
+                            <input type="text" name="input" />
+                            <button type="submit">Submit</button>
+                        </form>
+                    </body>
+                </html>
+            `;
+            return new Response(html, { headers: { "Content-Type": "text/html" } });
+        }
+
+        const initialBody = figlet.textSync("Hello, Bun!");
+        const html = `
+            <html>
+                <body>
+                    <pre>${initialBody}</pre>
+                    <form method="POST">
+                        <input type="text" name="input" />
+                        <button type="submit">Submit</button>
+                    </form>
+                </body>
+            </html>
+        `;
+        return new Response(html, { headers: { "Content-Type": "text/html" } });
     },
   });
   
